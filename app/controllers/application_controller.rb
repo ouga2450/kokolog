8af3fe49bb7747eeca93007156bed8b95e1d7ea3
ouga2450 <ouga.engneering@gmail.com>
2025-10-24 17:28:37 +0900

    fix:Lintチェック

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # サインアップ時（新規登録）
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
 
     # アカウント更新時（編集）
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
   end
 end
