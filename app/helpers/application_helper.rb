43117035a968375a8145e662118642b67fe6388f
ouga2450 <ouga.engneering@gmail.com>
2025-10-27 15:16:55 +0900

    fix:Lintチェック修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   include Heroicon::Engine.helpers
-  
+
   def icon(name, **options)
     heroicon(name, options: { class: "w-4 h-4 inline-block #{options[:class]}" })
   end
