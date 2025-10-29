193edbdbaf5813b73bf09a5c3e5dbfc7e25522c9
ouga2450 <ouga.engneering@gmail.com>
2025-10-29 14:39:58 +0900

    feat:feelingモデルの記述

--- app/models/feeling.rb
+++ app/models/feeling.rb
@@ -1,2 +1,5 @@
 class Feeling < ApplicationRecord
+  has_many :mood_logs, dependent: :nullify
+
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/mood.rb
+++ app/models/mood.rb
@@ -1,5 +1,5 @@
 class Mood < ApplicationRecord
-    has_many :mood_logs, dependent: :nullify
+  has_many :mood_logs, dependent: :nullify
 
   validates :score, presence: true, uniqueness: true
   validates :label, presence: true, uniqueness: true
