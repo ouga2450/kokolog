17256239be14f9b4a668dac2f96c04c28f7140bd
ouga2450 <ouga.engneering@gmail.com>
2025-10-30 17:46:11 +0900

    refactor: 認証処理とユーザーセットをApplicationControllerに集約

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :set_user
 
   def after_sign_in_path_for(resource)
     home_path
@@ -19,5 +20,11 @@ class ApplicationController < ActionController::Base
 
     # アカウント更新時（編集）
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
+  end
+
+  private
+
+  def set_user
+    @user = current_user
   end
 end
