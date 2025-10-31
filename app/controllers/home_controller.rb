cb24e450def1a881478ad6c118aaab532c12980b
ouga2450 <ouga.engneering@gmail.com>
2025-10-31 15:55:39 +0900

    feat: moodsテーブルのデータをview上で扱い、気分選択ボタンに表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,8 @@
 class HomeController < ApplicationController
   def index
+    # 全ての気分をスコア順に取得
+    @moods = Mood.order(:score)
     # ユーザーの気分登録を新しい順に取得
     @mood_logs = @user.mood_logs.includes(:mood).recent
-  end
-
-  private
-
-  def set_user
-    @user = current_user
   end
 end
