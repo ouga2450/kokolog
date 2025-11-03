1e1b5d35c9bc7838279415a6ec84f5ec56399d52
ouga2450 <ouga.engneering@gmail.com>
2025-11-03 09:41:01 +0900

    feat: 気分ログの編集

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     # 全ての気分をスコア順に取得
     @moods = Mood.order(:score)
-    # ユーザーの気分登録を新しい順に取得
-    @mood_logs = @user.mood_logs.includes(:mood).recent
+    # 今日のユーザーの気分登録を新しい順に取得
+    @mood_logs_today = current_user.mood_logs.includes(:mood).today.limit(10).recent
   end
 end
