2b45fe9acdfde2391f02dc559fd86f53772dc0e1
ouga2450 <ouga.engneering@gmail.com>
2025-11-03 15:16:09 +0900

    feat: 気分詳細記録実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,5 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "swiper/css"
 import "swiper/css/pagination"
+import ModalController from "./controllers/modal_controller"
+application.register("modal", ModalController)
