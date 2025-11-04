a48ce7a5f5b1f27b1f262ad929185b0efba08823
ouga2450 <ouga.engneering@gmail.com>
2025-11-04 18:53:43 +0900

    feat: モーダルによる記録した気分の詳細表示実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
     # 全ての気分をスコア順に取得
     @moods = Mood.order(:score)
     # 今日のユーザーの気分登録を新しい順に取得
-    @mood_logs_today = current_user.mood_logs.includes(:mood).today.limit(10).recent
+    @mood_logs_today = current_user.mood_logs.includes(:mood, :feeling).today.limit(10).recent
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,5 +3,3 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "swiper/css"
 import "swiper/css/pagination"
-import ModalController from "./controllers/modal_controller"
-application.register("modal", ModalController)
