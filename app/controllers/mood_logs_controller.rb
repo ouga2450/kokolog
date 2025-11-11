dd359a2e184e95a8f88268f3a45f256aaa152417
ouga2450 <ouga.engneering@gmail.com>
2025-11-11 22:40:30 +0900

    feat: 気分記録の削除機能実装

--- app/controllers/mood_logs_controller.rb
+++ app/controllers/mood_logs_controller.rb
@@ -27,29 +27,52 @@ class MoodLogsController < ApplicationController
     end
   end
 
-def update
-  @mood_log = current_user.mood_logs.find(params[:id])
+  def update
+    @mood_log = current_user.mood_logs.find(params[:id])
 
-  if @mood_log.update(mood_log_params)
-    flash.now[:notice] = "気分を更新しました。"
     respond_to do |format|
-      format.turbo_stream
-      format.html { redirect_back fallback_location: home_path, notice: "気分を更新しました。" }
-    end
-  else
-    respond_to do |format|
-      format.turbo_stream do
+      if @mood_log.update(mood_log_params)
+        flash.now[:notice] = "気分記録を更新しました。"
+        format.turbo_stream
+        format.html { redirect_back fallback_location: home_path, notice: "気分記録を更新しました。" }
+      else
         flash.now[:alert] = "更新に失敗しました。"
-        render turbo_stream: turbo_stream.replace(
-          "modal",
-          partial: "mood_logs/form",
-          locals: { mood_log: @mood_log }
-        )
+
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(
+            "modal",
+            partial: "mood_logs/form",
+            locals: { mood_log: @mood_log }
+          )
+        end
+        format.html { redirect_back fallback_location: home_path, alert: "更新に失敗しました。" }
       end
-      format.html { redirect_back fallback_location: home_path, alert: "更新に失敗しました。" }
     end
   end
-end
+
+  def destroy
+    @mood_log = current_user.mood_logs.find(params[:id])
+
+    respond_to do |format|
+      if @mood_log.destroy
+        flash.now[:notice] = "気分記録を削除しました。"
+
+        format.turbo_stream
+        format.html { redirect_back fallback_location: home_path, notice: "気分記録を削除しました。" }
+      else
+        flash.now[:alert] = "削除に失敗しました。"
+
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(
+            "modal",
+            partial: "mood_logs/form",
+            locals: { mood_log: @mood_log }
+          )
+        end
+        format.html { redirect_back fallback_location: home_path, alert: "削除に失敗しました。" }
+      end
+    end
+  end
 
   def mood_log_params
     params.require(:mood_log).permit(:mood_id, :feeling_id, :note, :recorded_at)
