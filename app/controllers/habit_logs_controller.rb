472ceb00facb09f4945f31178e3dd390c0722279
ouga2450 <ouga.engneering@gmail.com>
2025-11-23 20:15:15 +0900

    feat: 習慣実行画面モーダル表示確認

---
+++ app/controllers/habit_logs_controller.rb
@@ -0,0 +1,30 @@
+class HabitLogsController < ApplicationController
+
+  def new
+    @habit_log = HabitLog.new(
+      habit_id: params[:habit_id],
+      started_at: Time.current
+    )
+
+    # モーダル表示のため Turbo Frame を条件分岐
+    if turbo_frame_request?
+      render :new, layout: false
+    end
+  end
+
+  def create
+    @habit_log = current_user.habit_logs.build(habit_log_params)
+
+    if @habit_log.save
+      redirect_back fallback_location: home_path, notice: "習慣を記録しました。"
+    else
+      redirect_back fallback_location: home_path, alert: "記録に失敗しました。"
+    end
+  end
+
+  private
+
+  def habit_log_params
+    params.require(:habit_log).permit(:habit_id, :started_at, :ended_at, :memo)
+  end
+end
