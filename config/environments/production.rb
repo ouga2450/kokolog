f2a2b3af1674b0972995e025da0434a063821eae
ouga2450 <ouga.engneering@gmail.com>
2025-12-07 19:30:56 +0900

    fix: Lint修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,6 +103,6 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   # 本番環境も JST で動かす
-  config.time_zone = 'Asia/Tokyo'
+  config.time_zone = "Asia/Tokyo"
   config.active_record.default_timezone = :local
 end
