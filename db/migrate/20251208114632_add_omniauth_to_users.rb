cb704770fc780d8366c8b6868f8ade0ed47b9b4c
ouga2450 <ouga.engneering@gmail.com>
2025-12-09 21:31:01 +0900

    fix: 本番とローカルのDB差分吸収

--- db/migrate/20251208114632_add_omniauth_to_users.rb
+++ db/migrate/20251208114632_add_omniauth_to_users.rb
@@ -1,6 +1,7 @@
 class AddOmniauthToUsers < ActiveRecord::Migration[7.2]
   def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
+    # すでに存在する場合はスキップ（本番・ローカル差分対策）
+    add_column :users, :provider, :string, if_not_exists: true
+    add_column :users, :uid, :string, if_not_exists: true
   end
 end
--- db/migrate/20251208114654_add_index_uid_and_provider_to_users.rb
+++
@@ -1,4 +0,0 @@
-class AddIndexUidAndProviderToUsers < ActiveRecord::Migration[7.2]
-  def change
-  end
-end
