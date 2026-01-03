b3eb18f9b0a9fda65d49813ac85c0362aa9653b2
ouga2450 <ouga.engneering@gmail.com>
2026-01-03 13:12:52 +0900

    feat: マイページ実装

---
+++ app/controllers/mypages_controller.rb
@@ -0,0 +1,20 @@
+class MypagesController < ApplicationController
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    current_user.update!(user_params)
+    redirect_to mypage_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :bio)
+  end
+end
